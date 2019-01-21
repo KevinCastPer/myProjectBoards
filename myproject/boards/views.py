62cdbb8deeb00724338533f4355f9d3b2f160c5c
KevinCastPer <kecasper@gmail.com>
2019-01-21 11:52:20 +0100

    visualiza a plantilla con views

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Board
 
+
 def home(request):
     boards = Board.objects.all()
-    boards_names = list()
+    return render(request, 'home.html', {'boards': boards})
 
-    for board in boards:
-        boards_names.append(board.name)
-
-    response_html = '</br>'.join(boards_names)
-
-    return HttpResponse(response_html)
+# def home(request):
+#     boards = Board.objects.all()
+#     boards_names = list()
+#
+#     for board in boards:
+#         boards_names.append(board.name)
+#
+#     response_html = '</br>'.join(boards_names)
+#
+#     return HttpResponse(response_html)
